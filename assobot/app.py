c156bf3b4f3059c1866e12901189754b62a2e0d4
Nig3l <nig3lpro@gmail.com>
2022-03-25 11:04:21 +0100

    build(flask): init flask structure

--- assobot/app.py
+++ assobot/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Hello World from AssoBot!"
+   return render_template('index.html')
 
 def launch():
     app.run()
