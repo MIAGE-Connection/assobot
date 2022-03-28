44ce1c196561bad332966b95e5fc49273661fc52
Aurélien Bertrand <orelienbertrand@gmail.com>
2022-03-28 11:53:54 +0200

    feat: add route

--- assobot/app.py
+++ assobot/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
    return "Hello World from AssoBot!"
+
+@app.route("/plugins")
+def plugins():
+   return render_template('home.html')
 
 def launch():
     app.run()
