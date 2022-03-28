6533944c32ec5c468ce132c9a06d1ae4bb20fb12
Dawen Calippe <dawen.calp@gmail.com>
2022-03-28 15:35:02 +0200

    feat: authentification discord

--- assobot/app.py
+++ assobot/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
+
+from assobot.config import TOKEN, CLIENT_SECRET, REDIRECT_OAUTH_URL, REDIRECT_URL
+from zenora import APIClient
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "e5676d216b52c91d0c96750f781f445b730fd76f14315cc6358f7f647554f454"
+client = APIClient(TOKEN, client_secret=CLIENT_SECRET)
 
 @app.route("/")
 def home():
-   return render_template('index.html')
+    if 'token' in session:
+        bearer_client = APIClient(session.get('token'), bearer=True)
+        current_user = bearer_client.users.get_current_user()
+        return render_template('index.html', current_user=current_user)
+    return render_template('index.html', redirect_oauth_uri=REDIRECT_OAUTH_URL)
+
+@app.route("/oauth/callback")
+def callback():
+    code = request.args['code']
+    access_token =  client.oauth.get_access_token(code, REDIRECT_URL).access_token
+    session['token'] = access_token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 def launch():
     app.run()
---
+++ assobot/config.py
@@ -0,0 +1,6 @@
+from urllib import parse
+
+TOKEN="OTU3OTM4OTU1OTE4MTM1MzQ2.YkGELA.M1CLMonwQZ6ZBZIzYgrflpmrEBk"
+CLIENT_SECRET="M8v2APwEsqslYc78EsawJT5cTjTItrWa"
+REDIRECT_URL="http://127.0.0.1:5000/oauth/callback"
+REDIRECT_OAUTH_URL=f"https://discord.com/api/oauth2/authorize?client_id=957938955918135346&redirect_uri={parse.quote(REDIRECT_URL)}&response_type=code&scope=identify"
