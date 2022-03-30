6e78a4e52225286d27fbeb7484ee1b24f69634cb
Nig3l <nig3lpro@gmail.com>
2022-03-30 15:18:22 +0200

    refactor(plugin): Add Hello World exemple in default template plugin

--- assobot-sdk/plugin/plugin_template/plugin.py
+++ assobot-sdk/plugin/plugin_template/plugin.py
@@ -1,3 +1,6 @@
+import discord
+from discord.ext import commands
+
 from assobot.core.plugin import AbstractPlugin
 from .core import *
 
@@ -5,6 +8,12 @@ class Plugin(AbstractPlugin):
 
     def __init__(self) -> None:
         super().__init__('[[PLUGIN_NAME]]', '[[PLUGIN_DESCRIPTION]]')
+        self.__add_commands()
 
-    def init(self):
-        pass
+    def __add_commands(self):
+        self.bot.add_command(self.hello_world)
+
+    @commands.command()
+    async def hello_world(self, ctx, *args):
+        if not self.enabled: return
+        await ctx.channel.send(self.settings.get("hello-message"))
