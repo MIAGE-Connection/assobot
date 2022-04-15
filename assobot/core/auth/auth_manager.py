12db692247bbcbdcd038d6c7f937be3f934b1612
Clément QUERRE <clementquerrepro@gmail.com>
2022-04-15 13:27:56 +0200

    fix(plugin_reaction): Fix errors when forbidden API calls and remove possibility for user to make forbidden API calls

--- assobot/core/auth/auth_manager.py
+++ assobot/core/auth/auth_manager.py
@@ -22,7 +22,8 @@ class AuthManager:
     
     def get(self, user):
         if not self.__auth_manager.__contains__(user):
-            raise Exception('No user corresponding')
+            session.clear()
+            abort(500, 'Oups... Something went wrong... ')
         return self.__auth_manager[user]
 
     def get_current_ctx(self):
--- assobot_plugin/reaction_role/plugin.py
+++ assobot_plugin/reaction_role/plugin.py
@@ -1,12 +1,11 @@
-from assobot import CLIENT
+import emojis
+
 from assobot.core.auth.auth_manager import LOGGER
 import discord
 from discord.ext import commands
-import emojis
 from discord.ext.commands import has_permissions
 
 from assobot.core.plugin import AbstractPlugin
-from .core import *
 
 
 class reaction_rolePlugin(AbstractPlugin):
@@ -36,7 +35,6 @@ class reaction_rolePlugin(AbstractPlugin):
     async def on_raw_reaction_add(self, payload):
         if not payload.member.bot:
             guild = discord.utils.find(lambda g: g.id == payload.guild_id, self.bot.guilds)
-
             settings_manager = self.get_settings_manager(guild)
             if not bool(settings_manager.get("enabled")): return
 
@@ -51,7 +49,7 @@ class reaction_rolePlugin(AbstractPlugin):
             if role is not None:
                 member = payload.member
                 if member is not None:
-                    await member.add_roles(role)
+                    await self.try_to_add_or_remove_role(guild, member, role, payload.channel_id, False)
                 else:
                     LOGGER.error("Member not found")
             else:
@@ -75,7 +73,7 @@ class reaction_rolePlugin(AbstractPlugin):
 
         if role is not None:
             if member is not None:
-                await member.remove_roles(role)
+                await self.try_to_add_or_remove_role(guild, member, role, payload.channel_id, True)
             else:
                 LOGGER.error("Member not found")
         else:
@@ -87,7 +85,8 @@ class reaction_rolePlugin(AbstractPlugin):
 
         reaction_role_channel = discord.utils.get(ctx.guild.channels, id=channel_id)
 
-        reaction_role_message = settings_manager.get('reaction-role-message')
+        reaction_role_message = self.add_role_reaction_to_message(settings_manager, ctx.guild,
+                                                                  settings_manager.get('reaction-role-message'))
 
         sended_reaction_role_message = await reaction_role_channel.send(reaction_role_message)
         await self.add_all_reactions_to_message(settings_manager, sended_reaction_role_message)
@@ -95,14 +94,37 @@ class reaction_rolePlugin(AbstractPlugin):
         settings_manager.set('reaction-role-message-id', sended_reaction_role_message.id)
         settings_manager.save()
 
-    async def add_all_reactions_to_message(self, guild, settings_manager, message):
+    async def add_all_reactions_to_message(self, settings_manager, message):
         emoji_list = settings_manager.get('reaction-emoji-groups')
         for emoji in emoji_list:
             await message.add_reaction(emojis.encode(emoji))
-            
+
     def get_role_from_emoji(self, payload, guild):
         settings_manager = self.get_settings_manager(guild)
         emoji_list = settings_manager.get('reaction-emoji-groups')
         role_list = settings_manager.get('reaction-role-groups')
         index = emoji_list.index(emojis.decode(payload.emoji.name))
         return discord.utils.get(guild.roles, id=int(role_list[index]))
+
+    def add_role_reaction_to_message(self, settings_manager, guild, reaction_role_message):
+        emoji_list = settings_manager.get('reaction-emoji-groups')
+        role_list = settings_manager.get('reaction-role-groups')
+        for index, emoji in enumerate(emoji_list):
+            role = discord.utils.get(guild.roles, id=int(role_list[index]))
+            reaction_role_message += f'\n \t - {emoji} : {role.name}'
+        return reaction_role_message
+
+    async def try_to_add_or_remove_role(self, guild, member, role, channel_id, remove):
+        try:
+            if remove:
+                await member.remove_roles(role)
+            else:
+                await member.add_roles(role)
+        except:
+            if remove:
+                participe_passe = 'retiré'
+            else:
+                participe_passe = 'attribué'
+            error_message = f'Le role {role.name} ne peut pas être {participe_passe} car le role Assobot n\' est pas assez haut dans la hierarchie des rôles veuillez contacter un admin'
+            channel = discord.utils.get(guild.channels, id=channel_id)
+            await channel.send(error_message)
